e1f6efbbcb2e68dbb308ca7ffbc35b8f4dfafd97
Lorry Wei <58151836+theOGcat@users.noreply.github.com>
2022-01-05 01:35:51 -0500

    Footer update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import { ReactDOM } from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 
 
 function App(){
   return(
     <>
-
+ 
       <Router>
-        <Navbar/>
+      <Navbar/>
+      <Footer/>
         <Routes>
           <Route path ="/" exact/>
-        </Routes>
+        </Routes> 
       </Router>
-
     </>
   );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+Box,
+Container,
+Row,
+Column,
+FooterLink,
+Heading,
+} from "./FooterStyles.js";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "white",
+				textAlign: "center",
+				marginTop: "-50px" }}>
+		University of Windsor
+	</h1>
+	<Container>
+		<Row>
+		<Column>
+			<Heading>About Us</Heading>
+			<FooterLink href="#">Team</FooterLink>
+			<FooterLink href="#">Vision</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Services</Heading>
+
+			<FooterLink href="#">Teaching</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Contact Us</Heading>
+            <FooterLink href="#">Lorry Wei</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Social Media</Heading>
+			
+			<FooterLink href="#">
+			<i className="fab fa-twitter">
+				<span style={{ marginLeft: "10px" }}>
+				Twitter
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-youtube">
+				<span style={{ marginLeft: "10px" }}>
+				Youtube
+				</span>
+			</i>
+			</FooterLink>
+		</Column>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
