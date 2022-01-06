8e3c969d383af4f17977f0ae3b77466ffbe5e5e4
Lorry Wei <58151836+theOGcat@users.noreply.github.com>
2022-01-06 03:40:43 -0500

    About not fully edited, need to resolve nav css.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import { ReactDOM } from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import About from './components/About';
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 
 
 function App(){
   return(
     <>
- 
       <Router>
-      <Navbar/>
-      <Footer/>
-        <Routes>
-          <Route path ="/" exact/>
-        </Routes> 
+        <Navbar/>
+          <Routes>
+            <Route path ='/About' element={<About/>}/>
+          </Routes>
+          <Footer/>
       </Router>
     </>
   );
-
-
 }
 
 export default App;
