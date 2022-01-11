2fdd276b5ec22a51b0acb1790981940c224e7f42
Lorry Wei <58151836+theOGcat@users.noreply.github.com>
2022-01-11 03:19:10 -0500

    layout file created, integrated navbar and footer.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import { ReactDOM } from 'react';
 import './App.css'
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-// import About from './components/About';
+import Layout from './components/Layout';
 import HomeContent from './components/HomeContent';
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 import {Navigate} from 'react-router-dom';
+
 
 
 function App(){
   return(
     <>
       <Router>
-        <Navbar/>
+      <Layout>
           <Routes>
           <Route path ='/HomeContent' element={<HomeContent/>}/>
           {/* <Route path ='/About' element={<About/>}/> */}
-          <Route exact path="/" element={<Navigate to ='/HomeContent' replace />}/>
+          <Route exact path="/" element={<Navigate to ='/HomeContent' replace />}/>  
           </Routes>
-          <Footer/>
+          </Layout>
       </Router>
+
     </>
   );
 }
-//suck my d
 export default App;
