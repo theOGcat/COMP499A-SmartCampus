24ae145ae069a296e1d6f48208faa88b07be181c
Lorry Wei <58151836+theOGcat@users.noreply.github.com>
2022-02-27 00:43:25 -0500

    validate the Sign-In form component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,6 +13,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Alert from "@mui/material/Alert";
 import axois from 'axios';
 
 import { useNavigate } from "react-router-dom";
@@ -34,23 +36,35 @@ function Copyright(props) {
   const theme = createTheme();
   
   export default function SignIn() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isValidated, setValidation] = useState(true)
+
+
     const navigate = useNavigate()
     const handleSubmit = async (event) => {
       event.preventDefault();
       const data = new FormData(event.currentTarget);
       const customerEmail = data.get('email');
       const customerPassword = data.get('password');
+
+
+      if(customerEmail.length > 0 && customerPassword.length > 0 ){
+        setValidation(true)
+      }else{
+        setValidation(false)
+      }
+
+      if (isValidated){
       const res = await axois.post("http://localhost:3001/SignIn", {
           email: customerEmail,
           Password: customerPassword,
         });
         console.log(res);
         if(res.status === 200){
-          
-
           navigate('/Homepage')
         }
-     
+     }
       console.log({
         email: data.get('email'),
         password: data.get('password'),
@@ -75,6 +89,9 @@ function Copyright(props) {
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
+            { !isValidated && 
+                <Alert severity="error">Please Complete the Form</Alert>
+              }
             <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
               <TextField
                 margin="normal"
@@ -85,6 +102,9 @@ function Copyright(props) {
                 name="email"
                 autoComplete="email"
                 autoFocus
+                onBlur={(e) => setEmail(e.target.value)}
+                error={email === ""}
+                helperText={email === "" ? "Field is empty" : ""}
               />
               <TextField
                 margin="normal"
@@ -95,6 +115,9 @@ function Copyright(props) {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                onBlur={(e) => setPassword(e.target.value)}
+                error={password === ""}
+                helperText={password === "" ? "Field is empty" : ""}
               />
               <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
